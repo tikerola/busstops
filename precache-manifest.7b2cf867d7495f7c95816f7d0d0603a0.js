9e665902b268549b8464c63d9bcdb3a7e828d0a8
timo kerola <timo.kerola@pp2.inet.fi>
2020-05-03 20:05:33 +0300

    Updates

---
+++ precache-manifest.7b2cf867d7495f7c95816f7d0d0603a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1d2d883713241c1b1628f512d1680d",
+    "url": "/busstops/index.html"
+  },
+  {
+    "revision": "4224df7822efa016eefe",
+    "url": "/busstops/static/css/main.6e9c617a.chunk.css"
+  },
+  {
+    "revision": "10b50baac06b136b1f66",
+    "url": "/busstops/static/js/2.406ee924.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/busstops/static/js/2.406ee924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4224df7822efa016eefe",
+    "url": "/busstops/static/js/main.24013bf0.chunk.js"
+  },
+  {
+    "revision": "c758ab843591c7a2b33f",
+    "url": "/busstops/static/js/runtime-main.c5263070.js"
+  }
+]);
